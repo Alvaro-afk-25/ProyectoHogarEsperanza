63ff36d7d42baf186e5956c37de2ac1017837738
unknown <alvaro.guaman.est@tecazuay.edu.ec>
2021-05-04 12:08:02 -0500

    HTML de actividad-persona
    Creación de la tabla para el listado de actividades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
+import { ActividadPersonaComponent } from './components/actividad-persona/actividad-persona.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'actividad-persona',
+    component: ActividadPersonaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
